3e099fe31c7e1452ba44f99c09ab26b55b849cd9
stefanotuv <ste.tuveri@gmail.com>
2023-07-19 21:58:38 +0100

    first commit for login / register app. mail still not working

--- users_mgmt/settings.py
+++ users_mgmt/settings.py
@@ -20,7 +20,7 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development  development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
